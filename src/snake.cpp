981e8698bb983fdeb90c80542191bc818d8726cf
Barstok <mrbarstok@gmail.com>
2022-01-25 20:28:42 +0100

    test commit

--- src/snake.cpp
+++ src/snake.cpp
@@ -4,3 +4,23 @@ CSnake::CSnake(CRect r, char _c /*=' '*/):
   CFramedWindow(r, _c)
 {
 }
+void CSnake::printHelp(){
+	int x = geom.topleft.x, y = geom.topleft.y;
+	gotoyx(y + 2, x + 2);
+        printl("Use arrows to move snake");
+        gotoyx(y + 4, x + 2);
+        printl("Eat as much food as you can.");
+        gotoyx(y + 5, x + 2);
+        printl("Also, dont die.");
+        gotoyx(y + 7, x + 2);
+        printl("press 'p' or 'r' to play or 'h' for help");
+}
+
+void CSnake::paint(){
+	CFramedWindow::paint();
+	printHelp();
+}
+
+bool handleEvent(int key){
+	
+}
--- src/snake.h
+++ src/snake.h
@@ -2,12 +2,19 @@
 #define __SNAKE_H__
 
 #include "winsys.h"
+#include "cpoint.h"
+#include "screen.h"
 
 class CSnake:public CFramedWindow
 {
+private:
+	vector<CPoint> segments;
+	
 public:
   CSnake(CRect r, char _c = ' ');
-
+	void printHelp();
+	void paint();
+	bool handleEvent(int key);
 };
 
 #endif
